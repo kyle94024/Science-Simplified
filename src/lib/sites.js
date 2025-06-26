1c9db43984bde352db16fa782467f49a20efc7b3
Kyle Wan <kyle94024@gmail.com>
2025-06-26 10:59:14 -0700

    made als colors lighter

--- src/lib/sites.js
+++ src/lib/sites.js
@@ -473,10 +473,10 @@ export const sites = {
         primaryDark: "#E32636",
         lightGrey: "#f5f5f5",
         text: "#3d2447",
-        background: "#d5e4f5",
+        background: "#f5faff",
         contactUsColor: "#c6a5e3", //contactUsColor
         authorTextColor: "#6d32a8", //authorTextColor
-        footerBGColor: "#e08484", //footerBGColor
+        footerBGColor: "#f5c4c4", //footerBGColor
       },
       // Images (Home)
       homeBG: "ALS_bluecornflower.png", //home/homeBG
