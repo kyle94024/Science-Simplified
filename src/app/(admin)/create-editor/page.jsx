8395a63dbca057c49ffa7463ace17d42da12b101
Kyle Wan <kyle94024@gmail.com>
2025-07-20 17:51:43 -0700

    tried renaming scss

--- src/app/(admin)/create-editor/page.jsx
+++ src/app/(admin)/create-editor/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import "./CreateEditorPage.scss";
+import "./create-editor-page.scss";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
